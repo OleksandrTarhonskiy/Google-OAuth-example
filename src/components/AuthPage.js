39f72e53bb63b1fc688fb0b6a0eae7b1fe2a1415
Oleksandr Tarhonskiy <tarhonskiy.oleksandr@gmail.com>
2021-10-22 15:40:16 +0300

    Added login button

--- src/components/AuthPage.js
+++ src/components/AuthPage.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import GoogleLogin from 'react-google-login';
 
 function AuthPage(props) {
+  const getResponse = (res) => {
+    console.log(res);
+  }
   return (
     <div>
-      
+      <GoogleLogin
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        buttonText="Login"
+        onSuccess={getResponse}
+        onFailure={getResponse}
+        cookiePolicy={'single_host_origin'}
+        uxMode={'popup'}
+      />
     </div>
   );
 }
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { GoogleLogout } from 'react-google-login';
 
 function HomePage(props) {
+  const handleLogout = (res) => {
+    console.log(res);
+  };
+
   return (
     <div>
-      
+      <GoogleLogout
+        clientId={process.env.REACT_APP_CLIENT_ID}
+        buttonText="Logout"
+        onLogoutSuccess={handleLogout}
+      />
     </div>
   );
 }
